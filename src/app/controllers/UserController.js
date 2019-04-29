88251909a15522700fb64d4f5c301561351e8191
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2019-04-28 23:27:46 -0300

    Finalizada a parte do CRUD de Anuncios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,12 @@
 const User = require('../models/User')
 
 class UserController {
+  async index (req, res) {
+    const users = await User.find()
+
+    return res.json(users)
+  }
+
   async store (req, res) {
     const { email } = req.body
 
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,23 @@ const express = require('express')
 
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const controllers = require('./app/controllers')
 
 const authMiddleware = require('./app/middlewares/auth')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.get('/users', controllers.UserController.index)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ok:true}))
+routes.use(authMiddleware)
+
+/**
+ * Ads
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
