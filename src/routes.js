6acc232549d9052d7f04530d7ddd64b5c40da9df
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2019-05-01 19:21:55 -0300

    Adicionando Validators e Configurando os Middlewares de Validação

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
 const authMiddleware = require('./app/middlewares/auth')
 
-routes.get('/users', controllers.UserController.index)
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.get(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.index
+)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 routes.use(authMiddleware)
 
@@ -17,14 +31,18 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * Purchases
  */
 
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
