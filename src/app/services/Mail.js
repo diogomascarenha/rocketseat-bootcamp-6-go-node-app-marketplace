af8de4bc3152955e23eaf6ac13784f3c28269b5d
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2019-05-02 17:15:47 -0300

    Alterando bcrypt para bcryptjs, alterando imagem de node:10 para node:10-alpine e configurando o mailhog

--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
-
+console.log(mailConfig)
 const viewPath = path.resolve(__dirname, '..', 'views', 'emails')
 transport.use(
   'compile',
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,14 @@
+let auth = null
+if(process.env.MAIL_USERNAME &&  process.env.MAIL_PASSWORD) {
+  auth = {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD
+  }
+}
+
 module.exports = {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   secure: process.env.MAIL_SECRET,
-  auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD
-  }
+  auth
 }
